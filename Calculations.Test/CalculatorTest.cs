934febe32edc84591b0837aed6f450aa21129a58
JoaoLuizDev <joaoluizdeveloper@gmail.com>
2020-11-23 21:31:55 -0200

    Add project files.

---
+++ Calculations.Test/CalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Calculations.Tests/CalculatorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+namespace Calculations.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Add_GivenTwoIntValues_ReturnsInt()
+        {
+            var calc = new Calculator();
+            var result = calc.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+        
+        [Fact]
+        public void Add_GivenTwoDoubleValues_ReturnsDouble()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.5, 3.55);
+            Assert.Equal(4, result);
+            //Assert.Equal(4.78, result, 1);
+        }
+        
+        [Fact]
+        public double Add_GivenReturnsDouble()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.5, 2.5);
+            Assert.Equal(4, result);
+
+            return result;
+        }
+    }
+}
